b7bd2c48b6a3d1af0d38fa3cd5c3f052f9fd218e
house <house@DESKTOP-ND5IQSQ>
2019-04-16 13:49:17 +0200

    client swing

---
+++ src/main/java/fr/hoc/dap/swingclient/GmailPanel.java
@@ -0,0 +1,61 @@
+package fr.hoc.dap.swingclient;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ * The application DisplayClient
+ * displays mails & events from an user account on google
+ * it could add new user too !
+ * @author house_Mecrob
+ * @version 1.0
+ * @since 2019-03-21
+ */
+@SuppressWarnings("serial")
+public class GmailPanel extends JPanel implements ActionListener {
+
+    String nbUnread = null;
+    String URL = "http://localhost:8080//email/nbunread?userKey=";
+
+    private JLabel lbl2;
+    String userKey = "mecrob"; // TODO adresse en dur 
+
+    public GmailPanel() {
+
+        try {
+            nbUnread = ServerService.getData(URL + userKey);
+        } catch (Exception e) {
+            nbUnread = "error";
+        }
+
+        //On définit le layout à utiliser sur le content pane
+        this.setBackground(Color.LIGHT_GRAY);
+        this.setSize(200, 400);
+
+        JLabel lbl1 = new JLabel();
+        lbl1.setText("  Vous avez  :  ");
+        this.add(lbl1);
+
+        lbl2 = new JLabel();
+        lbl2.setText(nbUnread);
+        this.add(lbl2);
+
+        JLabel lbl3 = new JLabel("  email(s) non lu(s).");
+        this.add(lbl3);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        try {
+            lbl2.setText(ServerService.getData(URL + userKey));
+        } catch (Exception e1) {
+            lbl2.setText("error");
+        }
+    }
+
+}
---
+++ src/main/java/fr/hoc/dap/swingclient/MultiUser.java
@@ -0,0 +1,63 @@
+package fr.hoc.dap.swingclient;
+
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+/**
+* The application DisplayClient
+* displays mails&events from an user account on google
+* it could add new user too!*
+* @author house_Mecrob
+* @version 1.0
+* @since 2019-03-21
+*/
+public class MultiUser extends JFrame implements ActionListener {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    public JButton connect;
+    public JTextField user = new JTextField();
+    public static String userKey;
+
+    public MultiUser() {
+
+        connect = new JButton("Connection");
+        connect.addActionListener(this);
+        this.setSize(360, 100);
+        this.setLayout(new GridLayout());
+        this.setTitle("Connection compte utilisateur");
+        this.add(user);
+        this.add(connect);
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent event) {
+
+        Object source = event.getSource();
+
+        if (source == connect) {
+            String userKey = user.getText();
+            user.addActionListener(this);
+            connect.addActionListener(this);
+
+            try {
+                ServerService.getDataUnread(userKey);
+                ServerService.getDataEvent(userKey);
+                // TODO, ouvre les infos mail au premier clic, et event au deuxième. (normal l'adresse est en dur pour le display)
+
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+}
